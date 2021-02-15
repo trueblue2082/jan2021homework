d8569d1e2262cea1dda490c183eb2864c35db4f5
Donald Cole <77374737+trueblue2082@users.noreply.github.com>
2021-02-15 00:36:38 -0600

    Add files via upload

---
+++ Pizza Place.js
@@ -0,0 +1,6 @@
+const pizzaToppings = ["pepperoni", "sausage", "black olives", "anchovies"];
+let greetCustomer = "Welcome to Pizza Place, our toppings are pepperoni, sausage, black olives and anchovies";
+let getPizzaOrder = "size", "crust", "amount of toppings";
+  Size = 'small', 'medium', 'large'
+  crust = 'thin', 'deep dish', 'hand tossed'
+  amount of toppings = 'two per pizza';
